c376d74d503cc8e5055f5ab1467a48938ac806e9
DESKTOP-AI1BJP0\Noka <nguyenhieu01673@gmail.com>
2018-03-29 10:37:02 +0700

    Update FactoryMethod

--- RealFactoryMethod/Program.cs
+++ RealFactoryMethod/Program.cs
@@ -18,8 +18,21 @@ namespace RealFactoryMethod
         static void Main(string[] args)
         {
             // Note: constructors call Factory Method
+            Document[] documents = new Document[2];
 
+            documents[0] = new Resume();
+            documents[1] = new Report();
 
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 
@@ -55,6 +68,98 @@ namespace RealFactoryMethod
 
     }
 
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class InstroductionPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ResultsPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ConclusionPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class SummaryPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class BibliographyPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// The 'Creator' abstract class
+    /// </summary>
+    abstract class Document
+    {
+        private List<Page> _pages = new List<Page>();
+
+        // Constructor calls abstract Factory method
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+        public List<Page> Pages
+        {
+            get { return _pages; }
+        }
+
+        public abstract void CreatePages();
+
+    }
+
+    // <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Resume : Document
+    {
+        public override void CreatePages()
+        {
+            //throw new NotImplementedException();
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Report : Document
+    {
+        public override void CreatePages()
+        {
+            //throw new NotImplementedException();
+            Pages.Add(new InstroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
+        }
+    }
 
 
 }
