2f8c6017dff334e733bd18ec0c290af5db255310
DESKTOP-AI1BJP0\Noka <nguyenhieu01673@gmail.com>
2018-03-29 10:14:55 +0700

    update

---
+++ RealFactoryMethod/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealFactoryMethod
+{
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Factory Method Design Pattern.
+    /// </summary>
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Note: constructors call Factory Method
+
+
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+    abstract class Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class SkillsPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class EducationPage : Page
+    {
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ExperiencePage : Page
+    {
+
+    }
+
+
+
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Structural.Creatinonal.Singleton
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Singleton Design Pattern.
+    /// </summary>
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Object are the same instance");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// The 'Singleton' class
+        /// </summary>
+        class Singleton
+        {
+            private static Singleton _instance;
+
+            // Constructor is 'protected'
+            protected Singleton()
+            {
+
+            }
+
+            public static Singleton Instance()
+            {
+                // Uses lazy initialization.
+                // Note: this is not thread safe.
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
